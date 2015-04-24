5e3756db93f3070d5fd1aaf1919c939393954a93
Tobias Schulz <tobias.schulz186@gmail.com>
2015-04-24 02:08:31 +0200

    new projects

--- Calendar/Calendar.Exchange/Properties/AssemblyInfo.cs
+++ Calendar/Calendar.Exchange/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("Calendar")]
+[assembly: AssemblyTitle ("Calendar.Exchange")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
